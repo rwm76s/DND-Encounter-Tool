85c11fe7f506ae931c6bb34346a86703cdefa7b3
rmcgr <rileymcgraw2005@gmail.com>
2026-07-28 12:01:47 -0500

    Added landing page and redid login page css

--- src/main/java/itc475/dndencountertool/config/SecurityConfig.java
+++ src/main/java/itc475/dndencountertool/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/register", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
